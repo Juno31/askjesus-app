5e6100400ff240b7a86d3c6e0f6b3f7f8b8b95e0
Juno <dghsum@gmail.com>
2023-03-07 16:43:18 +0900

    [Chore] 프록시 설정 및 strictmode 변경

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: '/api',
+        destination: 'https://hollister-api-prod.creator.ly',
+      },
+    ];
+  },
+  env: {
+    SERVER_HOST: 'https://hollister-api-prod.creator.ly',
+    API_KEY: '$2b$10$.hDUtFyoMibYehgjHVbIpep5UEHeKHdhPkA9TFCiqlLlccP/c7Y6G',
+  },
+  reactStrictMode: false,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
