14d6d10a2783546f9877278b59387ee1abf42b30
Juno <dghsum@gmail.com>
2023-03-07 23:25:41 +0900

    [Design] 랜딩페이지 디자인 개선

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,8 @@ import Link from "next/link";
 //components
 import MessageContainer from "@/components/message/container";
 import MessageWrapper from "@/components/message/message-wrapper";
-import SystemBubble from "@/components/message/system";
-import MinBubble from "@/components/message/mine";
+import SystemBubble from "@/components/message/jesus-bubble";
+import MinBubble from "@/components/message/user-bubble";
 import Jesus from "@/components/message/jesus";
 
 //assets
@@ -19,18 +19,21 @@ const MESSAGE_TYPE = {
 function Home() {
   return (
     <div className="container m-auto flex h-max justify-center">
-      <div className=" flex w-full max-w-lg flex-col items-center bg-gradient-to-b from-purple-200 to-white pb-10">
-        <h1 className=" mt-20 text-center text-4xl font-bold text-purple-900">
-          Connect with <br />
-          Jesus <br />
-          AI Jesus Chatbot
+      <div className="bg-url fixed  min-w-full" />
+      <div className="max-w-kaya z-10 flex w-full flex-col items-center pb-10">
+        <h1 className=" mt-14 text-center text-4xl font-bold text-white md:mt-40">
+          Ask Jesus <br className="md:hidden" /> for Guidance
+          <br className="md:hidden" /> of your Life
         </h1>
-        <div className="flex min-w-full max-w-full justify-center">
-          <section className="mx-3 mt-16 flex w-full max-w-full flex-col gap-2 rounded-3xl bg-white p-6">
+        <h1 className=" mt-10 text-center text-2xl font-bold text-white">
+          AI Jesus Chatot
+        </h1>
+        <div className="z flex w-full min-w-full justify-center">
+          <section className="mx-3 mt-16 flex w-full max-w-full flex-col gap-2 rounded-3xl bg-white py-6 px-6 md:px-10">
             <MessageContainer>
               <Jesus />
               <MessageWrapper type={MESSAGE_TYPE.SYSTEM}>
-                <span className="text-sm font-bold text-black">Jesus</span>
+                <span className="text-kaya-black text-sm font-bold">Jesus</span>
                 <SystemBubble>{"Hi, I'm Jesus AI."}</SystemBubble>
                 <SystemBubble>Ask me anything</SystemBubble>
               </MessageWrapper>
@@ -42,28 +45,12 @@ function Home() {
             </MessageContainer>
             <Link
               href={"/jesus"}
-              className="mt-8 rounded-3xl bg-purple-600 px-16 py-4 text-center font-bold text-white"
+              className="bg-kaya-500 mt-8 rounded-3xl px-16 py-4 text-center font-bold text-white"
             >
               Start chat with Jesus
             </Link>
           </section>
         </div>
-        <p className="mt-5 font-light text-purple-600">
-          Give a friend a chance
-        </p>
-        <Link
-          href={"/jesus"}
-          className=" mt-2 rounded-3xl border-2 border-purple-600 bg-transparent px-16 py-4 text-center font-bold text-purple-600"
-        >
-          Start chat with Jesus
-        </Link>
-        <p className=" mt-20 min-w-full px-4 text-xs font-light text-purple-900">
-          Awake Corporation @2023
-          <br />
-          Seoul, South Korea
-          <br />
-          kim@creator.ly
-        </p>
       </div>
     </div>
   );
