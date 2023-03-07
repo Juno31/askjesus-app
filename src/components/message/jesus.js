d40940da21effa038672d54cc2321aa755ace605
Juno <dghsum@gmail.com>
2023-03-07 23:25:29 +0900

    [Fix] alt 값 추가

--- src/components/message/jesus.js
+++ src/components/message/jesus.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 function Jesus() {
   return (
     <div className="min-w-max">
       <Image
-        src={'/icons/jesus-icon.svg'}
+        src={"/icons/jesus-icon.svg"}
         width={48}
         height={48}
-        className={'min-w-6'}
+        className={"min-w-6"}
+        alt={"jesus image"}
       />
     </div>
   );
