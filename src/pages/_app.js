dad392bf16bf52a2c97316bb9edf36cb66e22653
Juno <dghsum@gmail.com>
2023-03-08 16:07:05 +0900

    [Fix] view height 오류 대응

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css';
+import "@/styles/globals.css";
+import "@/styles/custom.css";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  useEffect(function () {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    window.addEventListener("resize", function () {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
+
+    console.log(vh);
+  }, []);
+
   return <Component {...pageProps} />;
 }
