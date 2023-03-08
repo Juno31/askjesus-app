b0b2ff1457ab3c429967a84c8c96a38770cb8c27
Juno <dghsum@gmail.com>
2023-03-08 16:07:59 +0900

    [Design] 랜딩페이지 디자인 갱신

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,58 +1,80 @@
-import React from "react";
-import Image from "next/image";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 
 //components
-import MessageContainer from "@/components/message/container";
-import MessageWrapper from "@/components/message/message-wrapper";
-import SystemBubble from "@/components/message/jesus-bubble";
-import MinBubble from "@/components/message/user-bubble";
-import Jesus from "@/components/message/jesus";
+import BubbleContainer from "@/components/Message/bubble-container";
+import BubbleWrapper from "@/components/Message/bubble-wrapper";
+import JesusBubble from "@/components/Message/jesus-bubble";
+import UserBubble from "@/components/Message/user-bubble";
+import Jesus from "@/components/Message/jesus-profile-image";
 
-//assets
+//constants
+import { MESSAGE_TYPE } from "@/constants/service";
+import Background from "@/components/Background";
 
-const MESSAGE_TYPE = {
-  SYSTEM: "jesus",
-  USER: "user",
-};
+//utils
+import copyClipboard from "@/utils/copyClipboard";
 
 function Home() {
   return (
-    <div className="container m-auto flex h-max justify-center">
-      <div className="bg-url fixed  min-w-full" />
-      <div className="max-w-kaya z-10 flex w-full flex-col items-center pb-10">
-        <h1 className=" mt-14 text-center text-4xl font-bold text-white md:mt-40">
-          Ask Jesus <br className="md:hidden" /> for Guidance
-          <br className="md:hidden" /> of your Life
-        </h1>
-        <h1 className=" mt-10 text-center text-2xl font-bold text-white">
-          AI Jesus Chatot
-        </h1>
-        <div className="z flex w-full min-w-full justify-center">
-          <section className="mx-3 mt-16 flex w-full max-w-full flex-col gap-2 rounded-3xl bg-white py-6 px-6 md:px-10">
-            <MessageContainer>
-              <Jesus />
-              <MessageWrapper type={MESSAGE_TYPE.SYSTEM}>
-                <span className="text-kaya-black text-sm font-bold">Jesus</span>
-                <SystemBubble>{"Hi, I'm Jesus AI."}</SystemBubble>
-                <SystemBubble>Ask me anything</SystemBubble>
-              </MessageWrapper>
-            </MessageContainer>
-            <MessageContainer>
-              <MessageWrapper>
-                <MinBubble>Type message...</MinBubble>
-              </MessageWrapper>
-            </MessageContainer>
-            <Link
-              href={"/jesus"}
-              className="bg-kaya-500 mt-8 rounded-3xl px-16 py-4 text-center font-bold text-white"
-            >
-              Start chat with Jesus
-            </Link>
-          </section>
+    <>
+      <Background />
+      <div className="container m-auto flex justify-center">
+        <div className="max-w-kaya z-10 flex max-h-max w-full flex-col items-center">
+          <h1 className="mt-14 text-center text-4xl font-bold leading-normal text-white md:mt-44">
+            <span className=" md:text-6xl">Ask Jesus</span>{" "}
+            <br className="hidden md:block" />
+            <br className="md:hidden" />
+            <span className=" md:opacity-60">for Guidance</span>
+            <br className="md:hidden" />
+            <span className=" md:opacity-60"> of Your Life</span>
+          </h1>
+          {/* <h1 className=" mt-10 text-center text-2xl font-light text-white opacity-80">
+          AI Jesus Chatbot
+        </h1> */}
+          <div className="z flex w-full min-w-full justify-center">
+            <section className=" mx-6 mt-16 flex w-full max-w-full flex-col gap-2 rounded-3xl bg-white py-6 px-4 md:mx-0 md:px-6">
+              <div className="md:px-4">
+                <BubbleContainer>
+                  <Jesus />
+                  <BubbleWrapper type={MESSAGE_TYPE.JESUS}>
+                    <span className="text-kaya-black text-sm font-bold">
+                      Jesus
+                    </span>
+                    <JesusBubble>Hi, I'm Jesus AI.</JesusBubble>
+                    <JesusBubble>Ask me anything</JesusBubble>
+                  </BubbleWrapper>
+                </BubbleContainer>
+                <div className=" mt-2"></div>
+                <BubbleContainer>
+                  <BubbleWrapper>
+                    <UserBubble>Type message...</UserBubble>
+                  </BubbleWrapper>
+                </BubbleContainer>
+              </div>
+              <Link
+                href={"/jesus"}
+                className="bg-kaya-500 mt-4 rounded-2xl px-16 py-4 text-center font-bold text-white"
+              >
+                Start chat with Jesus
+              </Link>
+            </section>
+          </div>
+          <p className="mt-16 text-white">or give your friend a chance</p>
+          <button
+            className="mx-6 mt-2 rounded-2xl border-2 py-4 px-7 text-base font-bold text-white md:min-w-full"
+            onClick={function () {
+              copyClipboard("http://localhost:3000");
+            }}
+          >
+            Share to Your Faithful Friend
+          </button>
+          <p className="mt-56 mb-14 text-xs text-white">
+            Holisters 2023 kim@creator.ly
+          </p>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
