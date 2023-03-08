7a7c9258f34cb7318c0b7101bdca0ac53b077ec2
jhpung <62597091+jhpung@users.noreply.github.com>
2023-03-08 19:17:56 +0900

    fix: beusable

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import "@/styles/custom.css";
 import { useEffect } from "react";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   useEffect(function () {
@@ -13,5 +14,25 @@ export default function App({ Component, pageProps }) {
     });
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        id="beusable-script"
+        dangerouslySetInnerHTML={{
+          __html: `          
+(function(w, d, a){
+    w.__beusablerumclient__ = {
+        load : function(src){
+            var b = d.createElement("script");
+            b.src = src; b.async=true; b.type = "text/javascript";
+            d.getElementsByTagName("head")[0].appendChild(b);
+        }
+    };w.__beusablerumclient__.load(a + "?url=" + encodeURIComponent(d.URL));
+})(window, document, "//rum.beusable.net/load/b220608e154414u003");
+`,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
