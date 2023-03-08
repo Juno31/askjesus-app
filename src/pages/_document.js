7ffd1e29a1eea23515201c1b70790d54b7889e5a
jhpung <62597091+jhpung@users.noreply.github.com>
2023-03-08 19:45:54 +0900

    feat: add ga4

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,30 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { Script } from "next/script";
+import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <title>Ask Jesus</title>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-T3LCKS2W46"
+        ></Script>
+        <Script
+          id="google-analytics"
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-T3LCKS2W46');
+  `,
+          }}
+        ></Script>
       </Head>
       <body>
         <Main />
         <NextScript />
-
       </body>
     </Html>
   );
