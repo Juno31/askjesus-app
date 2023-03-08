11778676cf1344fe9a8e72a49a30bbd1dc2a7ef9
Juno <dghsum@gmail.com>
2023-03-08 16:20:45 +0900

    [Chore] FRONT_HOST 환경변수 추가

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const nextConfig = {
   // },
   env: {
     SERVER_HOST: "https://api.askjesus.me",
+    FRONT_HOST: "https://www.askjesus.me",
     API_KEY: "$2b$10$.hDUtFyoMibYehgjHVbIpep5UEHeKHdhPkA9TFCiqlLlccP/c7Y6G",
   },
   reactStrictMode: false,
