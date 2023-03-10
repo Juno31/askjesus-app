f1c0dadb2eadc70df4e8d11e0c839e1f36a7ab90
Juno <dghsum@gmail.com>
2023-03-10 14:49:42 +0900

    [Design] background color 추가

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+
 const Background = function () {
+  useEffect(function () {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--bg", `${vh}px`);
+
+    // window.addEventListener("resize", function () {
+    //   let vh = window.innerHeight * 0.01;
+    //   document.documentElement.style.setProperty("--bg", `${vh}px`);
+    // });
+  }, []);
   return <div className="bg-url max-h-view min-w-full object-contain" />;
 };
 
