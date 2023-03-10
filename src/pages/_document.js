2a4f9a53159f5f43fca2d47e5044ce80df959883
jhpung <62597091+jhpung@users.noreply.github.com>
2023-03-11 01:53:45 +0900

    feat: set theme

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,18 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="theme-color"
+          content="#221b31"
+          media="(prefers-color-scheme: light)"
+        />
+        <meta
+          name="theme-color"
+          content="#221b31"
+          media="(prefers-color-scheme: dark)"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
