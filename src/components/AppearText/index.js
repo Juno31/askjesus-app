a85e29f0e8bc2573f6fabcf0f39ec9a5ac86a5aa
Juno <dghsum@gmail.com>
2023-03-10 14:00:42 +0900

    [Update] 입장, 퇴장 문구 애니메이션 삭제

--- src/components/AppearText/index.js
+++ src/components/AppearText/index.js
@@ -5,14 +5,14 @@ function AppearText({ children }) {
 
   useEffect(function () {
     setTimeout(function () {
-      ref.current.classList.add("appear-animate");
-    }, 50);
+      ref.current.classList.add("opacity-100");
+    }, 500);
   }, []);
 
   return (
     <p
       ref={ref}
-      className="appear-target flex min-w-full flex-row items-center justify-center py-4 text-sm font-medium text-white md:text-base"
+      className="flex min-w-full flex-row items-center justify-center py-4 text-sm font-medium text-white opacity-0 md:text-base"
     >
       {children}
     </p>
