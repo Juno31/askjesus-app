7dba9e93abb5b3459061aee962fa3afb3810e0e8
Juno <dghsum@gmail.com>
2023-03-10 19:04:25 +0900

    [Chore] FRONT_HOST 변경

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
   // },
   env: {
     SERVER_HOST: "https://api.askjesus.me",
-    FRONT_HOST: "https://www.askjesus.me",
+    FRONT_HOST: "https://askjesus.me",
     API_KEY: "$2b$10$.hDUtFyoMibYehgjHVbIpep5UEHeKHdhPkA9TFCiqlLlccP/c7Y6G",
   },
   reactStrictMode: false,
