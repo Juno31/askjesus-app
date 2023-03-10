0c6b1c924655ac0035bf5fb4e5628a7ef3d4cf58
Juno <dghsum@gmail.com>
2023-03-10 13:59:38 +0900

    [Update] 텍스트 로딩 타임 갱신

--- src/utils/getTextLoadingTime.js
+++ src/utils/getTextLoadingTime.js
@@ -1,7 +1,7 @@
 const getTextLoadingTime = function (text) {
   if (!text.length) return 0;
 
-  return text.length * 50;
+  return text.length * (1000 / 15);
 };
 
 export default getTextLoadingTime;
