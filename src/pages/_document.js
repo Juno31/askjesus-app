041e33aa4e3de7e917687dc503ff7a91028a9e73
jhpung <62597091+jhpung@users.noreply.github.com>
2023-03-13 17:35:52 +0900

    chore: gtm test

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,6 +14,17 @@ export default function Document() {
           content="#221b31"
           media="(prefers-color-scheme: dark)"
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MRX3GMR');</script>
+<!-- End Google Tag Manager -->`,
+          }}
+        ></script>
       </Head>
       <body>
         <Main />
