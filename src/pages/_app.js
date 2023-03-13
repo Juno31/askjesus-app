80f5753bbac4687be9948737616bf80fb20b6711
jhpung <62597091+jhpung@users.noreply.github.com>
2023-03-13 17:01:19 +0900

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -72,15 +72,19 @@ export default function App({ Component, pageProps }) {
   `,
             }}
           />
-          <Script id="gtm-script" strategy="afterInteractive">
-            {`<!-- Google Tag Manager -->
+          <Script
+            id="gtm-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `<!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-MRX3GMR');</script>
-<!-- End Google Tag Manager -->`}
-          </Script>
+<!-- End Google Tag Manager -->`,
+            }}
+          ></Script>
         </>
       )}
 
